067b76dfdd0d782346269e23b488df9905afd1e5
Don <don@rumortech.com>
2019-12-15 12:07:51 -0700

    0.5.53.19 Move Object with Mouse
    Added mouse movement to the paddle.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float screenWidthInUnits = 16f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
     }
 }
