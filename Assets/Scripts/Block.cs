f1b714da889dc9ca49125163ebf94a96783605f1
Don <don@rumortech.com>
2019-12-20 01:38:39 -0700

    0.5.76.19 Added 2 and 3 hit blocks
    Made multi breakable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Block : MonoBehaviour
 {
-
+    // config parems
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     // Cached reference
     Level level;
+
+    // state variables
+    [SerializeField] int timesHit;  //for debug purposes
 
     private void Start()
     {
@@ -30,8 +36,27 @@ public class Block : MonoBehaviour
     {
         if (tag == "Breakable")
         {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
             DestroyBlock();
         }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
